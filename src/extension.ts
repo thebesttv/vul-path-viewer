0c32f79d6194511bebc6bb8c043a0446094c82ce
thebesttv <taojuntian@outlook.com>
2024-06-23 12:52:08 +0800

    Update label

--- src/extension.ts
+++ src/extension.ts
@@ -194,7 +194,7 @@ export function activate(context: vscode.ExtensionContext) {
                     // skip npe-good-source
                     .filter((result: any) => result.type !== 'npe-good-source')
                     .map((result: any) => ({
-                        label: `${result.type}: ${result.locations.length} stmts ${result.sourceIndex !== undefined ? result.sourceIndex : ''}`,
+                        label: `${result.type}: ${result.locations.length} stmts ${result.sourceIndex !== undefined ? '(' + result.sourceIndex + ')' : ''}`,
                         type: result.type,
                         collapsibleState: vscode.TreeItemCollapsibleState.None,
 
